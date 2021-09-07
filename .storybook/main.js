e74288d0be03c1f37acb294b62550b7593652d4e
Yotam Aran <yotam.sound@gmail.com>
2021-09-07 17:20:31 +0300

    storybook.test,Task finished,storybook configured

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,10 @@
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/components/**/*.stories.js",
   ],
+  "stories": ["../src/components/**/*.stories.js"],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
